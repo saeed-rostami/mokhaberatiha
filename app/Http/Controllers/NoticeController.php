f6873bba9449e82020c970bab83316a87415c5a2
sadra <sadra.sh.anjoman@gmail.com>
2024-08-29 01:11:08 +0330

    add this files

---
+++ app/Http/Controllers/NoticeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notice;
+use Illuminate\Http\Request;
+
+class NoticeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $notices = Notice::all();
+        return view('dashboard.notice.index', compact('notices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.notice.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'content' => 'required|string|max:255',
+            'title' => 'required|string|max:255',
+        ]);
+
+        // Use the validated data to create a new user
+        Notice::create($validatedData);
+
+        // Redirect or return a response
+        return redirect('notice')->with('success', 'اطلاعیه با موفقیت ثبت شد!');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
